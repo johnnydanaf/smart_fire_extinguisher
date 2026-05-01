c775b96668490f8aa2f81d5f2d53dca5d91b4360
maya-fakih <alfakihmaya1@gmail.com>
2026-05-01 12:56:59 +0300

    think snapshot updates
    needs rethinking if we should keep it only as a cached version of db calculation or only have it as it is at the moment an analyzer that inputs to the xgboost and it aligns the sensor snapshot and vision together based on their time stamps, then the think logs will have the output of the xg boost and used also as caching to save to the db? some tweaking still needs to be done to finalize the flow to make it as optimal as possible

--- src/think/snapshot.py
+++ src/think/snapshot.py
@@ -1,6 +1,8 @@
 import exceptions
 from dataclasses import dataclass, feild
 from datetime import datetime
+from sense import SensorSnapshot
+from see import VisionSnapshot
 
 @dataclass
 class ThinkSnapshot():
@@ -8,4 +10,19 @@ class ThinkSnapshot():
     timestamp: datetime
 
     # system snapshots
-    sensor_snapshot:
+    sensor_snapshot: SensorSnapshot
+    vision_snapshot: VisionSnapshot
+
+    # think derived
+    growth_rate: float      # derived through weighted calculation of the rate of all inputs
+    escelation_trend: str   # labeled by xgboost
+    danger_level: int
+    danger_label: str       # may be redundant if danger levels are enums
+    recommended_action: str # based on danger level and available hardware
+
+    # all vector points needed to be inputted to xgboost
+    # they are derived from db walkback of all saved sensor snapshots and vision snapshots saved
+    # calculations are never saved in the db bcz its faster to recompute, we can max cache them for speed
+    growth_rates: dict      # key = sensor name, value = velocity or growth rate
+    growth_trend: dict      # key = sensor name, value = acceleration or growth trend
+
