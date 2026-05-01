42b7d2dc4b527a78dc8e38e8506d0425a252836f
maya-fakih <alfakihmaya1@gmail.com>
2026-05-01 12:17:45 +0300

    see snapshot done

--- src/see/snapshot.py
+++ src/see/snapshot.py
@@ -30,4 +30,27 @@ class VisionSnapshot:
     glimpsed_fire: bool     # true if any fire box got detected by yolo even if low accuracy
     human_near_fire: bool   # if human label and fire whatsoever co-exist
 
+    # vision statistics to guide xgboost for a larger scale understanding
+    fire_count: int         # nb of bounding boxes with fire label
+    smoke_count: int        # nb of bounding boxes with smoke label
+    fire_union_area: float  # overlap_corrected union of fire bounding boxes
+    smoke_union_area: float # overlap_corrected union of smoke bounding boxes
+
+    # fire clusters
+    # a fire cluster is calculated after merging connected bounding boxes together to form a cluster
+    # having more than one cluster is a huge risk indicator
+    # model is expected to learn that higher cluster count means fire is speading
+    # dominant cluster is of index 0
+    cluster_count: int      # risk indicator
+    fire_clusters: list     # of type FireClusters ordered by danger score
+
+    image_url: str          # url to the captured frame saved to google dirve
+
+    raw_detections: list    # list of type Detection includes everything yolo returns for all selected bbxs
     
+
+
+    
+
+
+
